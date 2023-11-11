d1c3db1d150c4bc50bf69043b29a01b4854538fb
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2023-11-11 17:36:20 -0600

    First draft of game() completed

--- script.js
+++ script.js
@@ -13,6 +13,27 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return "Computer Wins"
     }
+}
+
+function game() {
+    
+    let playerScore = 0
+    let computerScore = 0
+    
+    for (let round=1; round<=5; round++) {
+        let userInput = prompt("Rock, Paper, or Scissors?")
+        let gameResult = playRound(userInput, getComputerChoice())
+        if (gameResult === "Player Wins") {
+            playerScore++
+            console.log("Player score increased")
+        } else if (gameResult === "Computer Wins") {
+            computerScore++
+            console.log("Computer score increased")
+        }
+        /*console.log(gameResult+" round "+round+"! Score is Player: "+playerScore+" Computer: "+computerScore)*/
+    }
+    console.log("Final Score: Player: "+playerScore+" Computer: "+computerScore)
+    
 
 
 
